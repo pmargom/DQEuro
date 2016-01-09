985bfab8c6c13b03a9f5a43c9c43ebbb4e82e04f
Pedro Martín <pmargom@gmail.com>
2016-01-09 10:48:41 +0000

    Nosotros page added

---
+++ Final/scripts/app.js
@@ -0,0 +1,45 @@
+
+angular.module("dqeuro", ["ngRoute", "angular-loading-bar"]);
+
+angular.module("dqeuro").config(function() {
+   
+   /*
+   BackendProvider.setApiKey(Properties.apiKey);
+   BackendProvider.setCorsRequests();
+   BackendProvider.setBackendUrl(Properties.backendUrl);
+   BackendProvider.setBackendImagesUrl(Properties.backendImagesUrl);
+   BackendProvider.setPersistedBackendUrl(Properties.persistedBackendUrl);
+   */
+   
+});
+
+angular.module("dqeuro").config(function(cfpLoadingBarProvider) {
+   
+   cfpLoadingBarProvider.includeSpinner = false;
+   
+});
+
+angular.module("dqeuro").config(function($routeProvider) {
+   
+   $routeProvider.when("/inicio", {
+      controller: "InicioCtrl",
+      templateUrl: "views/Home.html"
+   });
+
+   $routeProvider.when("/nosotros", {
+      controller: "NosotrosCtrl",
+      templateUrl: "views/AboutUs.html"
+   });
+
+   $routeProvider.when("/productos", {
+      controller: "ProductosCtrl",
+      templateUrl: "views/Products.html"
+   });
+
+   
+   // default route.
+   $routeProvider.otherwise({
+      redirectTo: "/inicio"
+   });
+   
+});
